f65f49ab04d817dca30f09f97face727a97484c3
primo79 <bsrodriguez87@ucatolica.edu.co>
2024-06-21 17:54:47 -0500

    Responsive page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaptopsComponent } from './components/laptops/laptops.component';
-import { CreateProductComponent } from './create-product/create-product.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LaptopsComponent,
-    CreateProductComponent
+  
   ],
   imports: [
     BrowserModule,
