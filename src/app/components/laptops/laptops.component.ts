2e98414b652c5c25a5ddc3fe5b05256ee04188a9
primo79 <bsrodriguez87@ucatolica.edu.co>
2024-06-22 01:45:19 -0500

    Retrieve info from the form

--- src/app/components/laptops/laptops.component.ts
+++ src/app/components/laptops/laptops.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-laptops',
@@ -6,10 +7,47 @@ import { Component } from '@angular/core';
   styleUrl: './laptops.component.css'
 })
 export class LaptopsComponent {
+  form: FormGroup;
+
   listLaptops: any[] = [
-    {Marca: 'Dell',Color: 'Blanco', Referencia:'1J2',Tamano:'12"', Almacenamiento:'128GB', Ram:'12GB',Procesador: 'Intel',Precio: '100.000'},
-    {Marca: 'Dell',Color: 'Blanco', Referencia:'1J2',Tamano:'12"', Almacenamiento:'128GB', Ram:'12GB',Procesador: 'Intel',Precio: '100.000'}
+    {Brand: 'Dell',Color: 'Blanco', Reference:'1J2',Size:'12"', Storage:'128GB', Ram:'12GB',Processor: 'Intel',Price: '100.000'},
+    {Brand: 'Dell',Color: 'Blanco', Reference:'1J2',Size:'12"', Storage:'128GB', Ram:'12GB',Processor: 'Intel',Price: '100.000'}
   ];
+
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      Brand: [''],
+      Color: [''],
+      Reference: [''],
+      Size: [''],
+      Storage: [''],
+      Ram: [''],
+      Processor: [''],
+      Price: [''],
+
+    })
+  }
+
+
+  ngOnInit(): void{
+
+  }
   
+    addlaptop(){
+      console.log(this.form);
+
+      const laptop: any = {
+        Brand: this.form.get('Brand')?.value,
+        Color: this.form.get('Color')?.value,
+        Reference: this.form.get('Reference')?.value,
+        Size: this.form.get('Size')?.value,
+        Storage: this.form.get('Storage')?.value,
+        Ram: this.form.get('Ram')?.value,
+        Processor: this.form.get('Processor')?.value,
+        Price: this.form.get('Price')?.value,
+      }
+      this.listLaptops.push(laptop)
+      this.form.reset();
+    }
 
 }
